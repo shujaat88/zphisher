35e41f46b9568e03e0b869c4d75d9d33d9b7b74d
shujaat88 <111701742+shujaat88@users.noreply.github.com>
2022-09-08 02:51:45 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -167,7 +167,7 @@ banner() {
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by Shujaat${WHITE}
 	EOF
 }
 
@@ -322,7 +322,7 @@ msg_exit() {
 about() {
 	{ clear; banner; echo; }
 	cat <<- EOF
-		${GREEN} Author   ${RED}:  ${ORANGE}TAHMID RAYAT ${RED}[ ${ORANGE}HTR-TECH ${RED}]
+		${GREEN} Author   ${RED}:  ${ORANGE}shujaat shafi ${RED}[ ${ORANGE}HTR-TECH ${RED}]
 		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/htr-tech
 		${GREEN} Social   ${RED}:  ${CYAN}https://tahmidrayat.is-a.dev
 		${GREEN} Version  ${RED}:  ${ORANGE}${__version__}
